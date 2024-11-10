06d37613661e886a6768f4d829a9edac5750b445
YoSoyElBot <yosoyelbot117@gmail.com>
2024-11-10 05:50:50 -0600

    Cambios terminados

---
+++ Services/PerfilClientService.cs
@@ -0,0 +1,10 @@
+namespace frontendnet.Services;
+
+
+public class PerfilClientService(HttpClient client)
+{
+    public async Task<string> ObtenTiempoAsync()
+    {
+        return await client.GetStringAsync($"api/auth/tiempo");
+    }
+}
